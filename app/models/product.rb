d14808dca199da55256fcb6a4fccfae3b1173828
Lecogoni <giraud.nicolas@me.com>
2021-03-08 00:02:16 +0100

    scaffold de Cart & LineItems avec les belongs necessaires

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,13 @@
 class Product < ApplicationRecord
+
+  has_many :line_items
+  
+  validates :title, :description, :price, :format presence: True
+  validates :title, length: {maximum: 140}
+  validates :description, length: {maximum: 1000}
+  validates :price, numericality: {only_integer: true}
+  
+  #const pour :format des Product
+  FORMAT = %w{png gif jpg}
+
 end
