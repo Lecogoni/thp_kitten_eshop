9120237bfd15f613c792a8139eddd4c21e007156
edmarsht <edtoulet@gmail..com>
2021-03-11 22:40:30 +0100

    Enlever active storage

--- app/controllers/avatar_controller.rb
+++ app/controllers/avatar_controller.rb
@@ -1,7 +1,2 @@
 class AvatarController < ApplicationController
-    def create
-        @product = Product.find(params[:product_id])
-        @product.avatar.attach(params[:avatar])
-        redirect_to(product_path(@product))
-      end
 end
